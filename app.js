f9f4d11a11854286dd4a655a291668e81b555877
iambariz <61651210+iambariz@users.noreply.github.com>
2021-01-19 14:58:58 +0100

    Mayor update

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 const buttons = document.querySelectorAll('.btn-custom');
 
+
+
 //console.log(buttons);
 
 buttons.forEach(item =>{
-    item.addEventListener('click', function(){
+    item.addEventListener('click', function(e){
+        e.preventDefault(); //Prevent refresh site on click
         alert("Coming soon!");
     })
 })
+
